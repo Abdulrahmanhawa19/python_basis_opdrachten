c716f2e2277dfee1d3a0b982be323248377d9834
Abdulrahmanhawa19 <97086714@st.deltion.nl>
2024-03-01 10:21:30 +0100

    opdr2

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -5,5 +5,10 @@
 # Hier komt je code...
 
 my_list = ...
+tekst = "Dit is een voorbeeldtekst waarin we het aantal keren dat de letter 't' voorkomt willen tellen."
+aantal_t = tekst.count("t")
+print("Het aantal keren dat de letter 't' voorkomt:", aantal_t)
 
-print()
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+aantal_t = my_string.count("t")
+print(f"De letter 't' komt {aantal_t}x voor in my_string")
--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,3 +3,12 @@
 # Groep:
 
 # Hier komt je code...
+for _ in range(5):
+    print("    *      " * 5)
+    print("   ***     " * 5)
+    print("  ******   " * 5)
+    print(" ********  " * 5)
+    print("***********" * 5)
+    print("    ***    " * 5)
+    print("    ***    " * 5)
+    print("    ***    " * 5)
