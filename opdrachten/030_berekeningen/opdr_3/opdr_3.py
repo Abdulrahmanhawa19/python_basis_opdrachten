8e5c00e7a068654cd0137df20040519fa0c03fe1
Abdulrahmanhawa19 <97086714@st.deltion.nl>
2024-03-06 11:36:16 +0100

    opdr 3

--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -7,7 +7,16 @@
 x = 1
 
 # Bereken y volgens de gegeven formule
-y = 4 * x**3 - 2 * x**2 - 1
+y = 4 * x**3 + 2 * x**2 + 2
 
-# Print de uitkomst
+# Geef de uitkomst weer
+print("De uitkomst is:", y)
+
+# Herhaal het proces voor andere waarden van x
+x = 2
+y = 4 * x**3 + 2 * x**2 + 5
+print("De uitkomst is:", y)
+
+x = 0
+y = 4 * x**3 + 2 * x**2 + 1
 print("De uitkomst is:", y)
--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,21 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+# Aanmaken van de list
+lijst = []
+
+# Aanmaken van de dictionaries voor de personen
+persoon1 = {"id": 1, "voornaam": "John", "achternaam": "Doe"}
+persoon2 = {"id": 2, "voornaam": "Jane", "achternaam": "Smith"}
+persoon3 = {"id": 3, "voornaam": "Alice", "achternaam": "Johnson"}
+persoon4 = {"id": 4, "voornaam": "Bob", "achternaam": "Brown"}
+
+# Toevoegen van de dictionaries aan de list
+lijst.append(persoon1)
+lijst.append(persoon2)
+lijst.append(persoon3)
+lijst.append(persoon4)
+
+# Afdrukken van de volledige naam van de 2e persoon
+print(lijst[4]["voornaam"], lijst[4]["achternaam"])
+
